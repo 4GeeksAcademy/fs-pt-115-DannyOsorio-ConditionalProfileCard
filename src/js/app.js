b1294752b8aaad1b9a750e1ff1b1b5ce502a48c2
OsorioDanny <daosva97@gmail.com>
2025-06-27 19:30:31 +0000

    finalizado

--- src/js/app.js
+++ src/js/app.js
@@ -30,17 +30,33 @@ function render(variables = {}) {
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
+
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
+  
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name}</h1>
+          <h1>${variables.lastName}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}.</h3>
+
+          <ul class=${
+            variables.socialMediaPosition === "position-left"
+              ? "position-left"
+              : "position-right"
+          }>
+            <li><a href="https://x.com/${
+              variables.twitter
+            }"><i class="fa-brands fa-x-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.github
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -64,11 +80,11 @@ window.onload = function() {
     github: null,
     linkedin: null,
     instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
+    name: "Insert your name",
+    lastName: "Insert you last name",
+    role: "choose your role",
+    country: "choose your country",
+    city: "choose your city"
   };
   render(window.variables); // render the card for the first time
 
